fe128b1365f02b4c0483ded44a9f9a9ef8ba1ff6
LAZYYY11 <gautampurvil@gmail.com>
2026-05-23 08:01:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Bot starting...");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`${client.user.tag} is online!`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+});
+
+client.login('MTQ3OTc2Mjc2MzIyMzAxMTQ4Mg.GRr7sg.XTfo8U2p2cAKKiqoKoFRZ30jZNiDbN6g_f2up4');
